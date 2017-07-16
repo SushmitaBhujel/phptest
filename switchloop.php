4e32e26785e5ec2c37d07373811e99881e0944e9
SushmitaBhujel <sushmita1997july@gmail.com>
2017-07-16 10:12:53 +0545

    changed to swichloop.php

---
+++ switchloop.php
@@ -0,0 +1,14 @@
+<?php
+$you = "fail again";
+
+switch ($you){
+    case  "fail":
+        echo "Workhard.";
+        break;
+    case "fail again":
+        echo "Work even harder.";
+        break;
+
+    default:
+        echo "Whatever happens in life never give up.";
+}
